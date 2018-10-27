3b5763b8a05d012b30965d3691a5f88f80ea4e2a
live-beta <wanjala.sam.s@gmail.com>
2018-10-27 19:58:28 -0400

    [update] update assets
    - update the asseets
    - Debug the application on fragment version

--- app/src/main/java/com/example/sam/moment/fragments/FirstClockFragment.java
+++ app/src/main/java/com/example/sam/moment/fragments/FirstClockFragment.java
@@ -9,7 +9,7 @@ import android.widget.ImageView;
 
 import com.example.sam.moment.R;
 
-public class FirstClockFragment extends Fragment{
+public class FirstClockFragment extends android.support.v4.app.Fragment {
     // extension how android knows how to implement the class
 
     // constructor
@@ -19,16 +19,12 @@ public class FirstClockFragment extends Fragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container
    , Bundle savedInstanceState) {
 
-        // code to inflate the body part layout
-
         View rootView = inflater.inflate(R.layout.fragment_clock_one,
                 container, false);
 
         ImageView imageView = rootView.findViewById(R.id.firstClock);
-
-        imageView.setImageResource(ImageAssets.getClocks);
+        imageView.setImageResource(R.mipmap.timer_primary);
         return rootView;
-
 
     }
 
